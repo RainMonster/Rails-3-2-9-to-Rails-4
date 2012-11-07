0a537c3b0ed4e36389c4af421e4d540b8e9f51a9
Nate Bird <natebird@abutler-e6420.mainoffice.local>
2012-11-07 10:49:04 -0500

    Rails 4 app copied over the top. Gemfile.lock isn't included yet.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_token is kept private
+# if you're sharing your code publicly.
 ExampleApp::Application.config.secret_token = 'a10d750d5d5c90fd80097e250a0aaae26399cd4ce43efb745014c63ff17fefc3decec8ecc8c68baf16ef83b6c5eb73985db65b94594bf3380f015c0d79c2b40e'
